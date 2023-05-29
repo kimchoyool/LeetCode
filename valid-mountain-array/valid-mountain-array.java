b9ad11dbaf1fe86610a2b5300abd71861f129a08
ChoYool <97028346+kimchoyool@users.noreply.github.com>
2023-05-29 19:47:24 +0900

    Time: 1 ms, Memory: 44.6 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.java
+++ valid-mountain-array/valid-mountain-array.java
@@ -1,26 +1,26 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
-        if (arr == null || arr.length < 3) { // edge case
+        if (arr == null || arr.length < 3) {
         return false;
         }
-        boolean isAsc = true;    //상승여부
+        boolean isAsc = true;   
         for (int i = 1; i < arr.length; i++) {
             if (isAsc) {
                 if (arr[i - 1] > arr[i]) {
-                    if (i == 1) { // 처음부터 하강이면 탈락
+                    if (i == 1) { 
                         return false;
                     } else {
-                        isAsc = false; // 하강 전환
+                        isAsc = false;
                     }
-                } else if (arr[i - 1] == arr[i]) { // 상승때 같은 경우 false
+                } else if (arr[i - 1] == arr[i]) { 
                     return false;
                 }
             } else { //하강
-                if (arr[i - 1] <= arr[i]) { // 하강시 뒷숫자가 크거나 같으면 false
+                if (arr[i - 1] <= arr[i]) { 
                     return false;
                 }
             }
         }
-        return isAsc? false: true; //하강없이 끝나면 산이 아니니 false 아니면 true;
+        return isAsc? false: true; 
     }
 }
